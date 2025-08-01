dad9337adc7d7728887d9715dd410fab9c58e12f
patled2000 <patled334@gmail.com>
2025-08-01 14:04:10 +0530

    Updated department icons and styles

--- src/components/IconBox/IconBoxStyle7.jsx
+++ src/components/IconBox/IconBoxStyle7.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import parser from 'html-react-parser';
 import { Link } from 'react-router-dom';
+ 
 
 export default function IconBoxStyle6({ title, subTitle, iconUrl, href }) {
   return (
-    <div className="cs_iconbox cs_style_7" style={{  }}>
-       <div className="cs_iconbox_icon">
+    <div className="cs_iconbox cs_style_7" >
+       <div className="cs_iconbox_icon" style={{
+  width: "60px",
+  height: "60px",
+  objectFit: "contain",
+  marginBottom: "16px"
+}}>
         <img src={iconUrl} alt="Icon" />
       </div> 
       <h2 className="cs_iconbox_title cs_fs_32" style={{color:"#000" , paddingRight:"0px"}}>{parser(title)}</h2>
